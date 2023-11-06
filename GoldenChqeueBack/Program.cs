30791d2d777c33ca01542ba7ed61a27f2fa69259
Ali <ali.nikfar2000@gmail.com>
2023-11-07 00:33:52 +0330

    AddScoped for Using RepositoryPattern

--- GoldenChqeueBack/Program.cs
+++ GoldenChqeueBack/Program.cs
@@ -37,6 +37,7 @@ var builder = WebApplication.CreateBuilder(args);
 //Sql Dependency Injection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IBankRepository, BankRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
