8297b81b20e4542afa37ba1dbe278abb5b7b91e3
Ali <ali.nikfar2000@gmail.com>
2023-12-01 23:13:26 +0330

    Add Authorize to bank controller

--- GoldenChqeueBack/Controllers/Api/BankApiController.cs
+++ GoldenChqeueBack/Controllers/Api/BankApiController.cs
@@ -1,6 +1,7 @@
 ﻿ using GoldenChequeBack.Domain.Entities;
 using GoldenChequeBack.Service.Contract;
 using GoldenChequeBack.Service.Contract.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,6 +10,7 @@ namespace GoldenChqeueBack.Controllers.Api
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BankApiController : ControllerBase
     {
         private readonly IBankRepository _bank;
